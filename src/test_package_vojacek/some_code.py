e0439f4a745913125897cb7570dd04cc4368204b
Libor Vojáček <vojaceklibor@gmail.com>
2024-03-10 23:05:20 +0100

    correct readthedocs.yaml

--- src/test_package_vojacek/some_code.py
+++ src/test_package_vojacek/some_code.py
@@ -1,12 +1,15 @@
 def foo(greeting="Hello world!"):
-    """Example foo function. Will identify itself and greet everyone.
+    """Foo function description ... bla bla text.
 
-    Args:
-        greeting (str, optional): The greeting string. Defaults to "Hello world!".
-
-    Returns:
-        int: 0 if successful
+    :param a: great parameter, very useful
+    :type a: int
+    :param c: _description_, defaults to [1,2]
+    :type c: list, optional
+    :raises AssertionError: _description_
+    :return: Returns 0 if everything's ok.
+    :rtype: int
     """
+
     print(f"This is foo function. {greeting}")
     return 0
 
