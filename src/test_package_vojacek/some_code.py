21a2149a79f3847eb0916b7a9be8bddbb173e7c5
Libor Vojáček <vojaceklibor@gmail.com>
2024-03-10 16:20:23 +0100

    more content to try automatic documentation with Sphinx

--- src/test_package_vojacek/some_code.py
+++ src/test_package_vojacek/some_code.py
@@ -1,1 +1,15 @@
-print("Here put some code to execute.")
+def foo(greeting="Hello world!"):
+    """Example foo function. Will identify itself and greet everyone.
+
+    Args:
+        greeting (str, optional): The greeting string. Defaults to "Hello world!".
+
+    Returns:
+        int: 0 if successful
+    """
+    print(f"This is foo function. {greeting}")
+    return 0
+
+
+if __name__ == "__main__":
+    foo()
